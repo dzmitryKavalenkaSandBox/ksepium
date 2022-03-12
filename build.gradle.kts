e61269ec52a622210c19d32ba0adb51401282956
Dzmitry Kavalenka <dzmitry_kavalenka@epam.com>
2022-03-12 12:43:44 -0600

    - Updated some gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ group = "org.dk.selenk"
 version = "1.0-SNAPSHOT"
 
 plugins {
-    kotlin("jvm") version "1.5.21"
+    kotlin("jvm")
 }
 
 repositories {
@@ -13,9 +13,9 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.dk.selenk:common:1.0-SNAPSHOT")
-    implementation("io.appium:java-client:7.5.1")
-    implementation("com.codeborne:selenide:5.23.0")
-    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.1.0")
+    implementation("io.appium:java-client:7.6.0")
+    implementation("com.code borne:selenide:5.23.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
     testImplementation("org.hamcrest:hamcrest-all:1.3")
-    testImplementation("io.mockk:mockk:1.11.0")
+    testImplementation("io.mockk:mockk:1.12.3")
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 rootProject.name = "ksepium"
 
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("org.jetbrains.kotlin.")) {
+                this.useVersion(gradle.rootProject.properties["kotlinVersion"].toString())
+            }
+        }
+    }
+}
